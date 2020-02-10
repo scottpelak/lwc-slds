f783948bb07e1812e4b6acda401ab4890cc35764
spelak-salesforce <spelak@salesforce.com>
2020-02-10 09:17:50 -0500

    eslintified lwc-services.config.js

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -1,8 +1,12 @@
 // Find the full example of all available configuration options at
 // https://github.com/muenzpraeger/lwc-create-app/blob/master/packages/lwc-services/example/lwc-services.config.js
+// eslint-disable-next-line no-undef
 module.exports = {
     resources: [
         { from: 'src/resources', to: 'dist/resources' },
-        { from: 'node_modules/@salesforce-ux/design-system/assets', to: 'dist/resources/slds' }
+        {
+            from: 'node_modules/@salesforce-ux/design-system/assets',
+            to: 'dist/resources/slds'
+        }
     ]
 };
